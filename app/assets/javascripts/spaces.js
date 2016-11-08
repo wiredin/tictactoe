87e58e6de6d360bee08d6da927a41f1766f0b282
Mike Weiss <michaelcweiss@gmail.com>
2016-11-07 22:21:37 -0500

    Checks for winner (horizonally)

--- app/assets/javascripts/spaces.js
+++ app/assets/javascripts/spaces.js
@@ -1,3 +1,7 @@
+$(document).on("click", "[data-behavior~=toggle_form_on_click]", function(event) {
+  event.preventDefault();
+  $(this).parent().find('.edit_space').submit();
+});
 $(document).on("ajax:complete", function(event) {
   Turbolinks.clearCache()
   Turbolinks.visit('/');
--- app/helpers/spaces_helper.rb
+++ app/helpers/spaces_helper.rb
@@ -5,4 +5,30 @@ module SpacesHelper
   def button_classes(space)
     ['no-btn', ('space-played' if played? space)]
   end
+
+  def last_player
+    Space.belonging_to(@session_user).order(:updated_at).last.player
+  end  
+
+  def current_player
+    last_player == 'x' ? 'o' : 'x'
+  end 
+
+  def game_over?(game)
+    @winner = winner(game) 
+    !@winner.nil?
+  end
+
+  def winner(game)
+    players = ['x','o']
+
+    #check horizontal
+    players.each do |p|
+      3.times do |i|
+        i = i * 3
+        return p if game[i..i+2].all? {|s| s.player == p} 
+      end
+    end
+    nil 
+  end 
 end
