d035fbf3aada6c07c77fc8d89de9cc248781ef5c
Mike Weiss <michaelcweiss@gmail.com>
2016-11-09 21:59:41 -0500

    Spaces can't be selected after game over

--- app/helpers/spaces_helper.rb
+++ app/helpers/spaces_helper.rb
@@ -22,13 +22,20 @@ module SpacesHelper
   def winner(game)
     players = ['x','o']
 
-    #check horizontal
     players.each do |p|
+      #check horizontal
       3.times do |i|
         i = i * 3
         return p if game[i..i+2].all? {|s| s.player == p} 
       end
+      #check vertical
+      3.times do |i|
+        return p if [game[i],game[i+3],game[i+6]].all? {|s| s.player == p}
+      end
+      #check diagonal
+      return p if [game[0],game[4],game[8]].all?  {|s| s.player == p}
+      return p if [game[2],game[4],game[6]].all?  {|s| s.player == p}
     end
-    nil 
+    return nil 
   end 
 end
